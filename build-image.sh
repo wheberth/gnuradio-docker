110693267f102785d6bbc0c4cbada4fe41764f51
Wheberth Dias <wheberth@gmail.com>
2021-07-24 21:11:53 +0200

    Initial commit, added Dockerfile for Gnuradio in Ubuntu 20.10

---
+++ build-image.sh
@@ -0,0 +1,1 @@
+sudo docker build -t gnuradio gnuradio
---
+++ run-gqrx.sh
@@ -0,0 +1,9 @@
+mkdir -p ~/gnuradio;
+docker run\
+    --device=/dev/dri \
+    --device=/dev/snd \
+    --privileged -v /dev/bus/usb:/dev/bus/usb \
+	-v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v ~/gnuradio:/home/gnuradio --ipc=host --net=host \
+    -e DISPLAY="$DISPLAY" gnuradio \
+    bash -c "source /pybombs/setup_env.sh; cd /home/gnuradio; gqrx";
